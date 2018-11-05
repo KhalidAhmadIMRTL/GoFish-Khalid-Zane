3b9c768cbffa6fd592ec5d96d4b14663444a1837
KhalidAhmad95 <khalid.ashraf.ahmad@gmail.com>
2018-11-05 12:23:13 -0600

    Updates on deck

--- deck.cpp
+++ deck.cpp
@@ -11,19 +11,51 @@
 using namespace std;
 
 Deck::Deck() {
-
+    myIndex = 0;
+    int numRanks = 13;
+    Card tempCard;
+    for (int j = 0; j < numRanks; j++) {
+        myCards[myIndex] = Card(j + 1, tempCard.spades);
+        myIndex++;
+    }
+    for (int j = 0; j < numRanks; j++) {
+        myCards[myIndex] = Card(j + 1, tempCard.hearts);
+        myIndex++;
+    }
+    for (int j = 0; j < numRanks; j++) {
+        myCards[myIndex] = Card(j + 1, tempCard.diamonds);
+        myIndex++;
+    }
+    for (int j = 0; j < numRanks; j++) {
+        myCards[myIndex] = Card(j + 1, tempCard.clubs);
+        myIndex++;
+    }
 }
-
+//8663088179
 void Deck::shuffle() {
-
+    //First create ordered set
+    Deck();
+    int numCards = SIZE;
+    //Set seed
+    srand(time(NULL));
+    //Swap cards
+    for (int i = 0 ; i < numCards ; i++){
+        int randomSwap = i + (rand() % (52 - i));
+        swap(myCards[i], myCards[randomSwap]);
+    }
 }
 
 Card Deck::dealCard() {
-
+    if(myIndex>0){
+        Card tempCard;
+        tempCard = myCards[myIndex];
+        myIndex --;
+        return (tempCard);
+    }
 }
 
 int Deck::size() const {
-    return 0;
+    return (SIZE - myIndex);
 }
 
 
